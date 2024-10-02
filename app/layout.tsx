95c084caf053f08af434e0940284ee9fca75ab29
Shailesh Thakur <thakurshaileshkumar507@gmail.com>
2024-10-03 01:33:09 +0530

    change push

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,10 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
     </ClerkProvider>
   );
 }
